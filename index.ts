ceaad4778eb265ab48d0e7eec627c1f7c3e7fe5b
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-10-30 19:56:30 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import versionControl from "./Utils/versionControl"
-import djsVersionControl from "./Utils/djsVersionControl"
 import ApplicationCommandCollector from "./Classes/ApplicationCommandCollector"
 import MessageCollector from "./Classes/MessageCollector"
 import BaseCollector from "./Classes/BaseCollector"
@@ -7,5 +6,4 @@ import colorette from "colorette"
 
 
 versionControl(colorette.red('This module version is not current. If you want to update: use "npm i djs-extended-collectors@latest" in your terminal.'))
-djsVersionControl(colorette.red('Discord.js version requires a minimum of 14.0.0 or higher. If you want to update: use "npm i discord.js@latest" in your terminal.'))
 export { ApplicationCommandCollector, MessageCollector, BaseCollector }
