60d2475d08ad2febd5da21b5f6090cd539c99ba1
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-11-16 12:27:29 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
-import versionControl from "./Utils/versionControl"
-import ApplicationCommandCollector from "./Classes/ApplicationCommandCollector"
-import MessageCollector from "./Classes/MessageCollector"
-import BaseCollector from "./Classes/BaseCollector"
-import colorette from "colorette"
+import BaseCollector from "./Classes/Bases/BaseCollector";
+import ApplicationCommandCollector from "./Classes/ApplicationCommandCollector";
+import AutocompleteCollector from"./Classes/AutoCompleteCollector";
+import MessageCollector from "./Classes/MessageCollector";
+import ModalSubmitCollector from "./Classes/ModalSubmitCollector";
+import awaitMessages from "./Functions/awaitMessages";
+import awaitModalSubmits from "./Functions/awaitModalSubmits";
+import awaitAutoCompletes from "./Functions/awaitAutoCompletes";
+import awaitApplicationCommands from "./Functions/awaitApplicationCommands";
+import versionControl from "./Utils/versionControl";
+import djsVersionControl from "./Utils/djsVersionControl";
+import CollectorTimer from "./Classes/Bases/CollectorTimer"
+import colorette from "colorette";
 
 
-versionControl(colorette.red('This module version is not current. If you want to update: use "npm i djs-extended-collectors@latest" in your terminal.'))
-export { ApplicationCommandCollector, MessageCollector, BaseCollector }
+
+versionControl(colorette.red("The version of this Module is out of date. To update it: npm i djs-extended-collectors@latest"))
+djsVersionControl(colorette.red("The version of Discord.js is not 14, To update it: npm i discord.js@14.x.x"))
+export { BaseCollector, ApplicationCommandCollector, AutocompleteCollector, MessageCollector, ModalSubmitCollector, awaitAutoCompletes, awaitApplicationCommands, awaitMessages, awaitModalSubmits, CollectorTimer }
