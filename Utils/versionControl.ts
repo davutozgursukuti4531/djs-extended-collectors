69400c9402b223554ead7e78927ea2397d433f9c
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-11-16 12:31:19 +0300

    Update versionControl.ts

--- Utils/versionControl.ts
+++ Utils/versionControl.ts
@@ -1,12 +1,13 @@
-import RexarTools from"rexar-tools"
+import RexarTools from "rexar-tools"
 
 
 
 
-const versionControl = async(warnMessage: string) => {
-    const thisModule = await RexarTools.npm("djs-extended-collectors")
-    if(thisModule.version !== require("../package.json").version){
-        console.log(warnMessage)
+const versionControl = async(warmMessage: string): Promise<void> => {
+    const thisVersion = require("../package.json").version
+    const latestVersion = await RexarTools.npm("djs-extended-collectors").then((m) => m.version)
+    if(thisVersion !== latestVersion){
+        console.log(warmMessage)
     } else {
         return;
     }
